1cd677372401ff182fc5295691d23ca90dfc5b02
jomarbarnobal <jomar.barnobal@outlook.com>
2015-06-18 05:01:49 +0800

    make it better

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  def send_devise_notification(notification, *args)
+	  if Rails.env.production?
+		  device_mailer.send(notification, self, *args).deliver_now
+	  else
+		  devise_mailer.send(notification, self, *args).deliver_later
+	  end
+  end
+end
